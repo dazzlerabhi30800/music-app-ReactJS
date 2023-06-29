cce5daefd39089e68f5cce9e7ab4fa6c232a72b8
= <abhisingh30800@gmail.com>
2023-06-29 10:39:14 +0530

    added error alert for token is undefined

--- src/Components/Pages/SearchPage/SearchBar.jsx
+++ src/Components/Pages/SearchPage/SearchBar.jsx
@@ -15,19 +15,23 @@ const SearchBar = ({
     setProgress(30);
     e.preventDefault();
     if (token !== undefined) {
-      setLoading(true);
-      const data = await getRecentData(searchInput, token);
-      searchRef.current.blur();
-      setProgress(50);
-      setTimeout(() => {
+      try {
+        setLoading(true);
+        const data = await getRecentData(searchInput, token);
+        searchRef.current.blur();
+        setProgress(50);
+        setTimeout(() => {
+          setProgress(100);
+          setLoading(false);
+          setSearchData(data.tracks.items);
+        }, 800);
+      } catch (error) {
+        alert("You are not signed In");
         setProgress(100);
-        setLoading(false);
-        setSearchData(data.tracks.items);
-      }, 800);
-    } else {
-      alert("Sign in To search");
+      }
     }
   }
+
   return (
     <div className="search--wrapper">
       <form onSubmit={handleSubmit}>
